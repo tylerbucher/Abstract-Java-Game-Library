3f9edfccf2b6637821f2fc8ae4172ee8f6f3ed02
Tyler Bucher <tylerb318@gmail.com>
2015-05-13 13:36:05 -0500

    Finished Quad Collision

--- src/main/java/org/ajgl/collision/axisaligned/Quadrilateral.java
+++ src/main/java/org/ajgl/collision/axisaligned/Quadrilateral.java
@@ -2,21 +2,70 @@ package org.ajgl.collision.axisaligned;
 
 public class Quadrilateral {
 
+    /**
+     * 0---3
+     * |   |
+     * |   |
+     * 1---2
+     * 
+     * @param vertices
+     * @param values
+     * @return
+     */
     public static boolean vQuadIntersect2D(double[] vertices, double... values) {
-        return vertices[4] < values[0] || vertices[0] > values[4] 
-                || vertices[5] > values[1] || vertices[1] < values[5];
+        return vertices[4] > values[0] || vertices[0] < values[4] 
+                || vertices[3] < values[1] || vertices[1] > values[3];
     }
     
+    /**
+     * 0---3
+     * |   |
+     * |   |
+     * 1---2
+     * 
+     * @param vertices
+     * @param values
+     * @return
+     */
     public static boolean vQuadContains2D(double[] vertices, double... values) {
         return values[0] >= vertices[0] && values[1] <= vertices[1]
-                && values[6] <= vertices[6] && values[7] >= vertices[7];
+                && values[4] <= vertices[4] && values[5] >= vertices[5];
     }
     
+    /**
+     * 
+     *    4---7
+     *   /|  /|
+     *  0---3 |
+     *  | 5-|-6
+     *  |/  |/ 
+     *  1---2
+     * 
+     * @param vertices
+     * @param values
+     * @return
+     */
     public static boolean vQuadIntersect3D(double[] vertices, double... values) {
-        return false;
+        return vertices[6] > values[0] || vertices[0] < values[6] ||
+                vertices[4] < values[1] || vertices[1] > values[4] ||
+                vertices[14] > values[2] || vertices[2] < values[14];
     }
     
+    /**
+     * 
+     *    4---7
+     *   /|  /|
+     *  0---3 |
+     *  | 5-|-6
+     *  |/  |/ 
+     *  1---2
+     * 
+     * @param vertices
+     * @param values
+     * @return
+     */
     public static boolean vQuadContains3D(double[] vertices, double... values) {
-        return false;
+        return values[0] >= vertices[0] && values[1] <= vertices[1] && values[2] >= vertices[2]
+                && values[18] <= vertices[18] && values[19] >= vertices[19] && values[20] <= vertices[20];
     }
 }
